efe17deac339024f22d312353367fbdbed6fb507
sprhawk <465558+sprhawk@users.noreply.github.com>
2019-06-14 23:30:58 +0800

    added elpa into repo

---
+++ elpa/django-snippets-20131229.1611/django-snippets-autoloads.el
@@ -0,0 +1,34 @@
+;;; django-snippets-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "django-snippets" "django-snippets.el" (0 0
+;;;;;;  0 0))
+;;; Generated autoloads from django-snippets.el
+
+(autoload 'django-snippets-initialize "django-snippets" "\
+
+
+\(fn)" nil nil)
+
+(eval-after-load 'yasnippet '(django-snippets-initialize))
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "django-snippets" '("django-snippets-dir")))
+
+;;;***
+
+;;;### (autoloads nil nil ("django-snippets-pkg.el") (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; django-snippets-autoloads.el ends here
---
+++ elpa/django-snippets-20131229.1611/django-snippets-pkg.el
@@ -0,0 +1,10 @@
+(define-package "django-snippets" "20131229.1611" "Yasnippets for django"
+  '((yasnippet "0.8.0"))
+  :authors
+  '(("Yasuyuki Oka" . "yasuyk@gmail.com"))
+  :maintainer
+  '("Yasuyuki Oka" . "yasuyk@gmail.com")
+  :url "https://github.com/myfreeweb/django-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
